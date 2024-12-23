10b5bea87ddea279a447d02ab919e9be34476974
whatphilipcodes <gerdes.philip@gmail.com>
2024-12-23 18:28:41 +0100

    fx: scrolling

--- src-front/App.tsx
+++ src-front/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import ConnectButton from "./ConnectButton";
+import taupyIcon from "./assets/taupy.svg";
 function App() {
 
   return (
     <main className="container" style={{ textAlign: "left" }}>
-      <p >The Python API is starting in the background. This demo setup is not showing any loading state. Because of that, inital connection attemps might fail until the uvicorn server has been launched successfully.</p>
+      <div>
+        <img src={taupyIcon} style={{ width: 80 }} />
+        <p >The Python API is starting in the background. This demo setup is not showing any loading state. Because of that, inital connection attemps might fail until the uvicorn server has been launched successfully.</p>
+      </div>
       <ConnectButton />
     </main >
   );
